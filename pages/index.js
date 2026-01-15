71841b99c9da405298f8f3fd094b95d6747722df
Jonatas Gomes Cerqueira <96083946+johngoc@users.noreply.github.com>
2026-01-15 00:52:03 +0000

    adiciona scripts `lint:check e lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
-function Home () {
-    return <h1> Gabrielly Ribeiro Parreira, se me ama mesmo dá uma risadinha! </h1>
+function Home() {
+  return (
+    <h1> Gabrielly Ribeiro Parreira, se me ama mesmo dá uma risadinha! </h1>
+  );
+}
+
+function teste() {
+  console.log("identação errada");
 }
 
 export default Home;
